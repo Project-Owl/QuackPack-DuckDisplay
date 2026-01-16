7c0be7a796c27bc5dfd6b28f3281233454d0a3e4
Brenton Poke <brentonpoke@outlook.com>
2026-01-16 12:21:15 -0500

    constructors
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- examples/BasicDuckFirmware/src/main.cpp
+++ examples/BasicDuckFirmware/src/main.cpp
@@ -1,3 +1,12 @@
 //
 // Created by brent on 1/16/2026.
 //
+#include <DuckDisplay.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
--- src/DuckDisplay.h
+++ src/DuckDisplay.h
@@ -2,14 +2,24 @@
 #ifndef DUCKDISPLAY_H
 #define DUCKDISPLAY_H
 #include <cstdint>
+#include <Adafruit_SSD1306.h>
 #include <Wire.h>
 
-template <class  Display>
+template <class  Display = Adafruit_SSD1306>
 class DuckDisplay {
 public:
-    DuckDisplay() : width(128), height(64) {
-
+    DuckDisplay() : width(128), height(64), sda(21), scl(22) {
+        Wire.begin(sda, scl);
+        display = Display(width, height, &Wire, -1);
     };
+    DuckDisplay(int width, int height) : width(width), height(height), sda(21), scl(22) {
+        Wire.begin(sda, scl);
+        display = Display(width, height, &Wire, -1);
+    }
+    DuckDisplay(int width, int height, int sda, int scl) : width(width), height(height), sda(sda), scl(scl) {
+        Wire.begin(sda, scl);
+        display = Display(width, height, &Wire, -1);
+    }
     void showDefaultScreen();
 
     std::uint8_t getWidth() const {return width;}
@@ -17,6 +27,7 @@ public:
 protected:
     std::uint8_t width, height;
 private:
-    Display display(width, height, &Wire, -1);
+    int sda, scl;
+    Display display;
 };
 #endif
