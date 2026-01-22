9c4e20935fb957432b81b2e6b5094416f178521b
Brenton Poke <brentonpoke@outlook.com>
2026-01-22 14:20:57 -0500

    preparing for U8G2
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- src/DuckDisplay.cpp
+++ src/DuckDisplay.cpp
@@ -2,15 +2,17 @@
 // Created by brent on 1/16/2026.
 //
 #include "DuckDisplay.h"
-#include <Adafruit_SSD1306.h>
+#include "U8g2lib.h"
 
-template<class Display>
-void DuckDisplay<Display>::showDefaultScreen() {
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    display.println("MamaDuck Device");
-    display.println("Initializing...");
-    display.display();
-}
+class U8G2Display : public DuckDisplay<U8G2> {
+public:
+    ~U8G2Display() override = default;
+    void showDefaultScreen() override {
+        display.clearDisplay();
+        display.setFont(u8g2_font_ncenB08_tr);
+        display.setCursor(0, 10);
+        display.println("MamaDuck Device");
+        display.println("Initializing...");
+        display.display();
+    }
+};
--- src/DuckDisplay.h
+++ src/DuckDisplay.h
@@ -8,24 +8,34 @@
 template <class  Display = Adafruit_SSD1306>
 class DuckDisplay {
 public:
-    DuckDisplay():width(128), height(64), sda(21), scl(22) {
+    DuckDisplay():width(128), height(64), sda(21), scl(22), rst_pin(-1) {
         Wire.begin(sda, scl);
-        display = Display(width, height, &Wire, -1);
+        DuckDisplay<Display>::begin();
     }
-    DuckDisplay(int width, int height) : width(width), height(height), sda(21), scl(22) {
+    DuckDisplay(int width, int height) : width(width), height(height), sda(21), scl(22), rst_pin(-1) {
         Wire.begin(sda, scl);
-        display = Display(width, height, &Wire, -1);
+        DuckDisplay<Display>::begin();
     }
-    DuckDisplay(int width, int height, int sda, int scl, int rst_pin = -1) : width(width), height(height), sda(sda), scl(scl) {
+    DuckDisplay(int width, int height, int sda, int scl, int reset_pin = -1) : width(width), height(height), sda(sda), scl(scl), rst_pin(reset_pin) {
         Wire.begin(sda, scl);
-        display = Display(width, height, &Wire, rst_pin);
+        DuckDisplay<Display>::begin();
     }
     virtual ~DuckDisplay() = default;
-    virtual void showDefaultScreen();
+    virtual void showDefaultScreen()    {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0);
+        display.println("MamaDuck Device");
+        display.println("Initializing...");
+        display.display();
+    }
     virtual void begin() {
         if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
             logdbg_ln("SSD1306 allocation failed");
         }
+
+        display = Display(width, height, &Wire, rst_pin);
         showDefaultScreen();
     }
     virtual void clear() {
@@ -42,8 +52,7 @@ public:
     [[nodiscard]] std::uint8_t getHeight() const {return height;}
 protected:
     std::uint8_t width, height;
-private:
-    int sda, scl;
+    int sda, scl, rst_pin;
     Display display;
 };
 #endif
