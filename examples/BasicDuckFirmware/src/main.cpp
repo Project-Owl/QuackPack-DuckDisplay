bb6c20b2b34042091c39a40367e6fdca054b0b3b
Brenton Poke <brentonpoke@outlook.com>
2026-01-25 02:08:20 -0500

    adding a delay
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- examples/BasicDuckFirmware/src/main.cpp
+++ examples/BasicDuckFirmware/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <DuckDisplay.h>
 
-DuckDisplay<Adafruit_SSD1306> display;
+DuckDisplay<> display;
 void setup() {
     display.clear();
     display.getDisplay().setTextSize(1);
@@ -12,6 +12,7 @@ void setup() {
 }
 
 void loop() {
+    delay(1000);
     display.clear();
     display.getDisplay().setCursor(0,10);
     display.getDisplay().print("Looping");
