fec05486b29d8cfdf4f33262f4c0669bc12da7e5
Brenton Poke <brentonpoke@outlook.com>
2026-01-27 23:33:13 -0500

    triaging
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- examples/BasicDuckFirmware/src/main.cpp
+++ examples/BasicDuckFirmware/src/main.cpp
@@ -1,19 +1,26 @@
-#include <Arduino.h>
 #include <DuckDisplay.h>
 
-DuckDisplay<> display;
+//DuckDisplay<Adafruit_SSD1306> display;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+DuckDisplay display(SCREEN_WIDTH, SCREEN_HEIGHT, 21, 22, OLED_RESET);
 void setup() {
-    display.clear();
+    // Initialize the display before first use and verify it succeeded.
+    display.begin(OLED_RESET);
+    display.getDisplay().clearDisplay();
     display.getDisplay().setTextSize(1);
     display.getDisplay().setTextColor(SSD1306_WHITE);
     display.getDisplay().setCursor(0,0);
     display.getDisplay().print("Test");
+    display.getDisplay().display();
 
 }
 
 void loop() {
     delay(1000);
-    display.clear();
+    display.getDisplay().clearDisplay();
     display.getDisplay().setCursor(0,10);
-    display.getDisplay().print("Looping");
+    display.getDisplay().println("Looping");
+    display.getDisplay().display();
 }
--- src/DuckDisplay.h
+++ src/DuckDisplay.h
@@ -4,24 +4,24 @@
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 #include "utils/DuckLogger.h"
-#include "Ducks/DuckTypes.h"
 #include <string>
-#include "include/cdpcfg.h"
 
 template <class  Display = Adafruit_SSD1306>
 class DuckDisplay {
 public:
-    DuckDisplay():width(128), height(64), sda(21), scl(22), rst_pin(-1) {
-        Wire.begin(sda, scl);
-        DuckDisplay<Display>::begin(0x3C);
+    DuckDisplay() : width(128), height(64), sda(21), scl(22), rst_pin(-1), duckType(DuckType::MAMA) {
+        //Wire.begin(sda, scl);
+        //DuckDisplay<Display>::begin(0x3c);
     }
-    DuckDisplay(int width, int height) : width(width), height(height), sda(21), scl(22), rst_pin(-1) {
-        Wire.begin(sda, scl);
-        DuckDisplay<Display>::begin(0x3C);
+
+    DuckDisplay(int width, int height) : width(width), height(height), sda(21), scl(22), rst_pin(-1), duckType(DuckType::MAMA) {
+        //Wire.begin(sda, scl);
+        //DuckDisplay<Display>::begin(0x3C);
     }
-    DuckDisplay(int width, int height, int sda, int scl, int reset_pin = -1) : width(width), height(height), sda(sda), scl(scl), rst_pin(reset_pin) {
-        Wire.begin(sda, scl);
-        DuckDisplay<Display>::begin(0x3C);
+
+    DuckDisplay(int width, int height, int sda, int scl, int reset_pin = -1) : width(width), height(height), sda(sda), scl(scl), rst_pin(reset_pin), duckType(DuckType::MAMA) {
+        //Wire.begin(sda, scl);
+        //DuckDisplay<Display>::begin(0x3C);
     }
     virtual ~DuckDisplay() = default;
 
@@ -30,14 +30,14 @@ public:
      * @return void
      * @brief Displays the default screen on the OLED display
      */
-    virtual void showDefaultScreen()    {
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setTextColor(SSD1306_WHITE);
-        display.setCursor(0, 0);
-        display.println("MamaDuck Device");
-        display.println("Initializing...");
-        display.display();
+    virtual void showDefaultScreen(){
+        this->display.clearDisplay();
+        this->display.setTextSize(1);
+        this->display.setTextColor(SSD1306_WHITE);
+        this->display.setCursor(0, 10);
+        this->display.println("MamaDuck Device");
+        this->display.println("Initializing...");
+        this->display.display();
     }
     /**
      * @param i2c_addr
@@ -46,12 +46,11 @@ public:
      * initialization, so you must override that method if using a different display library.
      */
     virtual void begin(uint8_t i2c_addr) {
-        display = Display(width, height, &Wire, rst_pin);
-        if(!display.begin(SSD1306_SWITCHCAPVCC, i2c_addr)) {
+        Wire.begin(sda, scl);
+        Serial.begin(115200);
+        this->display = Display(width, height, &Wire, rst_pin);
+        if(!this->display.begin(SSD1306_SWITCHCAPVCC, i2c_addr))
             logerr_ln("SSD1306 allocation failed");
-            return;
-        }
-        showDefaultScreen();
     }
     /**
      * @param none
@@ -78,14 +77,28 @@ public:
         display.ssd1306_command(SSD1306_DISPLAYON);
     }
 
-    [[nodiscard]] uint8_t getWidth() const {return width;}
-    [[nodiscard]] uint8_t getHeight() const {return height;}
+    [[nodiscard]] uint16_t getWidth() const {return width;}
+    [[nodiscard]] uint16_t getHeight() const {return height;}
     /**
      * @param none
-     * @return Display&
+     * @return Display
      * @brief Returns a reference to the display object for direct manipulation
      */
-    Display& getDisplay() {return display;}
+    Display getDisplay() {return display;}
+
+    enum DuckType {
+        /// A Duck of unknown type
+        UNKNOWN = 0x00,
+        /// A PapaDuck
+        PAPA = 0x01,
+        /// A MamaDuck
+        MAMA = 0x02,
+        /// A DuckLink
+        LINK = 0x03,
+        /// A Detector Duck
+        DETECTOR = 0x04,
+        MAX_TYPE
+      };
     /**
      * @param duckType
      * @return std::string
@@ -113,7 +126,7 @@ public:
     }
 protected:
     uint8_t width, height;
-    int sda, scl, rst_pin, duckType{0x02};
+    int sda, scl, rst_pin, duckType;
     Display display;
 };
 #endif
