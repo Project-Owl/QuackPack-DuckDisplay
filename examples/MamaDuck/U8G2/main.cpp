f1073297ffb2ff2ab4afb4f500e5a756424d4f1e
Brenton Poke <brentonpoke@outlook.com>
2026-02-22 00:08:40 -0500

    class templating to allow of all of U8G2's driver classes
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- examples/MamaDuck/U8G2/main.cpp
+++ examples/MamaDuck/U8G2/main.cpp
@@ -5,8 +5,9 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    // 16 heltec v2
 #define SDA_PIN 21 //4 heltec v2
-#define SLC_PIN 22 //15 heltec v2
-U8G2Display disp(SCREEN_WIDTH, SCREEN_HEIGHT, U8X8_PIN_NONE);
+#define SCL_PIN 22 //15 heltec v2
+ U8G2_SSD1306_128X64_NONAME_F_HW_I2C driver(U8G2_R0,U8X8_PIN_NONE, SCL_PIN, SDA_PIN);
+U8G2Display<U8G2_SSD1306_128X64_NONAME_F_HW_I2C> disp(SCREEN_WIDTH, SCREEN_HEIGHT,driver, U8X8_PIN_NONE);
 
 void setup() {
     // Initialize the display before first use and verify it succeeded.
--- src/U8G2Display.h
+++ src/U8G2Display.h
@@ -4,30 +4,38 @@
 #include "U8g2lib.h"
 #include <utils/DuckLogger.h>
 #include "utils/DuckUtils.h"
-
+template <class Driver>
 class U8G2Display : public DuckTest<U8G2> {
 public:
     U8G2Display()
         : DuckTest() {
-        this->display = U8G2(U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0,U8X8_PIN_NONE, this->scl, this->sda));
     }
-    U8G2Display(int width, int height, int rst_pin = -1)
+
+    U8G2Display(Driver& driver)
+        : DuckTest() {
+        this->display = U8G2(driver);
+    }
+    U8G2Display(int width, int height,Driver& driver, int rst_pin = -1)
         : DuckTest (width,
           height,
           rst_pin) {
-        this->display = U8G2(U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0,U8X8_PIN_NONE, this->scl, this->sda));
+        this->display = U8G2(driver);
     }
 
-    U8G2Display(int width, int height, int sda, int scl, uint8_t i2caddr, int rst_pin = -1)
+    U8G2Display(int width, int height, int sda, int scl, uint8_t i2caddr,Driver& driver, int rst_pin = -1)
         : DuckTest (width,
           height,
           sda,
           scl,
           i2caddr,
           rst_pin) {
-        this->display = U8G2(U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0,U8X8_PIN_NONE, this->scl, this->sda));
+        this->display = U8G2(driver);
     }
     ~U8G2Display() override = default;
+    void setDriver(Driver& driver) {
+        this->display = U8G2(driver);
+    }
+
     void showDefaultScreen() override {
         loginfo_ln("Showing default U8G2 screen");
         display.clearDisplay();
